c66a9bfe7767a6151c7de8489a4341a8a311dc16
shoegazeboi <2003dims@gmail.com>
2021-01-21 02:19:10 +0300

    shell_scripts

---
+++ shell_scripts/makemigrations.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /home/eva-01/PycharmProjects/django/project
+python manage.py makemigrations
+python manage.py migrate
---
+++ shell_scripts/runserver.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /home/eva-01/PycharmProjects/django/project
+python manage.py runserver
